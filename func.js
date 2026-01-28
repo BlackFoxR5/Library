79f0eeb4c21e64e500ec6d3ce654b6ce1ec086b5
BlackFoxR5 <anshumayborah99@gmail.com>
2026-01-28 16:22:30 +0530

    remade from scratch

--- func.js
+++ func.js
@@ -1,169 +1,217 @@
-console.log('Small library app');
+console.log("from scratch");
 
-const library = []; // the books will stored in this array
+const myLibrary = []; //empty array that will store the books
 
-//book constructor
-function Book(title, author, published, hasRead) {
+//contructor to create new books
+function Book(title, author, yearPublished, hasRead) {
     this.title = title;
     this.author = author;
-    this.published = published;
+    this.yearPublished = yearPublished;
     this.hasRead = hasRead;
 
-    this.uniqueId = crypto.randomUUID(); //generates a unique ID for each book
+    this.id = crypto.randomUUID();
 }
 
-//method added to the book constructor to switch read status
-// Book.prototype.toggleRead = function() {
-//     if(this.hasRead === 'Yes') {
-//         this.hasRead = 'No';
-//     } else if(this.hasRead === 'No') {
-//         this.hasRead = 'Yes';
-//     }
-// }
-
-//function to create the book and store it in the empty array
-function booksToLibrary(title, author, published, hasRead) {
-    let newBook = new Book(title, author, published, hasRead); //calling the book constructor and saving the object in the declared variable
-    library.push(newBook); //adding the object to the 'library' array
+//calls the constructor and adds them in the library
+function addBooksToLibrary(title, author, yearPublished, hasRead) {
+    let newBook = new Book(title, author, yearPublished, hasRead);
+    myLibrary.push(newBook);
 }
 
-//books are created
-booksToLibrary('Fooled by Randomness', 'Nassim Nicholas Taleb', 2001, 'Yes');
-booksToLibrary("1984", "George Orwell", 1949, "No");
-booksToLibrary("The Hobbit", "J.R.R. Tolkien", 1937, "No");
-booksToLibrary('Thus Spoke Zarathustra', 'Friedrich Nietzsche', 1883, 'Yes');
+//new book with arguments added to the empty array
+addBooksToLibrary("Ghost in the wires", "Kevin Mitnick", 2001, "No");
+addBooksToLibrary("Fooled by Randomness", "Nassim Nicholas Taleb", 2004, "Yes");
 
-let container = document.querySelector('.container');
+//loops over all the elements in the array and display them in the page
 
-//function that loops through the array and displays each book on the page
-function sortInPage(array) {
+//separates each book from array
+function displayBooks(array) {
     for(let i = 0; i < array.length; i++) {
         let book = array[i];
-        console.log(book.title, book.author, book.published, book.hasRead, book.uniqueId);
-
-        let bookInfo = document.createElement("div"); //the div where the book content will be displayed
-        bookInfo.setAttribute('data-uniqueId', book.uniqueId); //unique id of the book set to the div
-
-        let titleElement = document.createElement("h1");
-        let titleStrong = document.createElement("strong");
-        titleStrong.textContent = book.title;
-        titleElement.appendChild(titleStrong); //strong p for the title
+        // console.log(book.title, book.author, book.yearPublished, book.hasRead, book.id);
         
+        let bookCard = document.createElement("div"); //cards for the books to shown in
+        let container = document.querySelector(".container");
+        container.appendChild(bookCard); //cards are added to the container
+
+        bookCard.dataset.unique = book.id; //id of each books added to the DOM
+
+        let titleElement = document.createElement("h1"); //h1 for title
+        titleElement.textContent = book.title;
+        bookCard.appendChild(titleElement); //title is shown in the card
+
         let authorElement = document.createElement("p");
         authorElement.textContent = `by ${book.author}`; //p for author
-        
-        let publishedElement = document.createElement("p");
-        publishedElement.textContent = `Published: ${book.published}`; //p for year published
-    
-        let readElement = document.createElement("p");
-        readElement.textContent = `Read: ${book.hasRead}`; //p for read status
-        
-        //adds all the p into the div
-        bookInfo.appendChild(titleElement);
-        bookInfo.appendChild(authorElement);
-        bookInfo.appendChild(publishedElement);
-        bookInfo.appendChild(readElement);
-        
-        let removeBtn = document.createElement('button');
-        removeBtn.classList.add('removeBtn');
-        removeBtn.textContent = 'Remove Book';
-        removeBtn.setAttribute('data-uniqueId', book.uniqueId) //unique id of the set to the remove button
-        bookInfo.appendChild(removeBtn); //remove button added to each book card
+        bookCard.appendChild(authorElement); //author is shown in card
 
-        //adds div into the page
-        container.appendChild(bookInfo);
+        let yearPublished = document.createElement("p");
+        yearPublished.textContent = `Published in: ${book.yearPublished}`;
+        bookCard.appendChild(yearPublished); //year of publication shown in card
+
+        let readStatus = document.createElement("p");
+        readStatus.textContent = `Read status: ${book.hasRead}`;
+        bookCard.appendChild(readStatus); //read status shown in card
+
+        //remove button
+        let removeBtn = document.createElement("button");
+        removeBtn.classList.add("removeBtn");
+        removeBtn.textContent = "Remove Book";
+        bookCard.appendChild(removeBtn);
+
+        //toggle read status button
+        let toggleBtn = document.createElement("button");
+        toggleBtn.classList.add("toggleBtn");
+        toggleBtn.textContent = "Toggle read status";
+        bookCard.appendChild(toggleBtn);
     }
 }
 
-//re-write again
-container.addEventListener('click', (e) => {
-    if(e.target.classList.contains('removeBtn')) {
-        const bookId = e.target.getAttribute('data-uniqueId');
+displayBooks(myLibrary); //displays all the books in the library in the webpage
 
-        const index = library.findIndex(book => book.uniqueId === bookId);
-        if(index !== -1) {
-            library.splice(index, 1);
-        }
+let addBook = document.querySelector(".newBook"); //refers to the new book button
+let showForm = document.querySelector('.askUser'); //refers to the dialog that opens when clicked on new book
 
-        const bookCard = container.querySelector(`[data-uniqueId='${bookId}']`);
-        if(bookCard) {
-            bookCard.remove();
-            alert('book removed');
-        }
-    }
-});
-//re-write again
-
-sortInPage(library); //this places the books in the page and display them in the console
-
-let addBookButton = document.querySelector('.newBook');
-let showForm = document.querySelector('.askUser');
-let confirmBtn = showForm.querySelector('.confirmBtn');
-let cancelBtn = showForm.querySelector('.cancelBtn');
-
-addBookButton.addEventListener('click', () => {
-    // alert('Hooray! you clicked the button!!!');
+//opens a form to add a new book
+addBook.addEventListener("click", () => {
     showForm.showModal();
 });
 
-cancelBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    alert('No books added');
-    showForm.close();
+let confirm = document.querySelector(".confirmBtn");
+let cancel = document.querySelector(".cancelBtn");
+
+//add book in the array and then show it in the page
+confirm.addEventListener("click", (e) => {
+    e.preventDefault(); //default action normally taken by the implementation as a result of the event will not occur
+
+    let title = document.querySelector("#titleInput").value.trim(); //trim() removes whitespaces from the input. eg "hey ".trim() === "hey"
+    let author = document.querySelector("#authorInput").value.trim();
+    let yearPublished = Number(document.querySelector("#publishedInput").value.trim()); //converting string to integer
+    let readStatus = document.querySelector("#readInput").value.trim();
+
+    if(title === "" || author === "" || yearPublished === "") {
+        alert("Form cannot be empty!");
+        return false;
+    } else {
+        addBooksToLibrary(title, author, yearPublished, readStatus);
+    
+        // console.log(title, author, yearPublished, readStatus, myLibrary[myLibrary.length - 1].id);
+    
+        let bookCard = document.createElement("div"); //cards for the books to shown in
+        let container = document.querySelector(".container");
+        container.appendChild(bookCard); //cards are added to the container
+
+        bookCard.dataset.unique = myLibrary[myLibrary.length - 1].id; //id of the last added book is added to DOM
+
+        let titleElement = document.createElement("h1"); //h1 for title
+        titleElement.textContent = title;
+        bookCard.appendChild(titleElement); //title is shown in the card
+
+        let authorElement = document.createElement("p");
+        authorElement.textContent = `by ${author}`; //p for author
+        bookCard.appendChild(authorElement); //author is shown in card
+
+        let yearPublished1 = document.createElement("p");
+        yearPublished1.textContent = `Published in: ${yearPublished}`;
+        bookCard.appendChild(yearPublished1); //year of publication shown in card
+
+        let readStatus1 = document.createElement("p");
+        readStatus1.textContent = `Read status: ${readStatus}`;
+        bookCard.appendChild(readStatus1); //read status shown in card
+
+        //remove button
+        let removeBtn = document.createElement("button");
+        removeBtn.classList.add("removeBtn");
+        removeBtn.textContent = "Remove Book";
+        bookCard.appendChild(removeBtn);
+
+        //toggle read status button
+        let toggleBtn = document.createElement("button");
+        toggleBtn.classList.add("toggleBtn");
+        toggleBtn.textContent = "Toggle read status";
+        bookCard.appendChild(toggleBtn);
+
+        // console.table(myLibrary); //debugging
+        alert("New book added");
+        showForm.close(); //closes the form 
+
+        document.querySelector("#bookForm").reset(); //clears the form from previous inputs
+    }
 });
 
-confirmBtn.addEventListener('click', (e) => {
+//remove form after cancel button is clicked
+cancel.addEventListener("click", (e) => {
     e.preventDefault();
-    alert('Book added!');
     showForm.close();
+    alert("No books added");
+});
 
-    //saving the input value to use it as params to create book in library
-    const title = document.getElementById("titleInput").value;
-    const author = document.getElementById("authorInput").value;
-    const published = document.getElementById("publishedInput").value;
-    const hasRead = document.getElementById("readInput").value;
+//remove button
+let mainContainer = document.querySelector(".container"); //refers to the main container which displays the book cards
 
-    booksToLibrary(title, author, published, hasRead); //adds new book to library
+mainContainer.addEventListener("click", (e) => {
+    if(!e.target.classList.contains("removeBtn")) //checks whether the button clicked contains the class "removeBtn"
+        return;
 
-    //add the single new book entered by user in modal form
-    let singleBookInfo = document.createElement("div");
+    //search for the unique id related to the card in which the "remove book" button is clicked
+    let individualBook = e.target.closest("[data-unique]"); //finds the nearest element that has a "data-unique" attribute
+    //console.log(individualBook);
 
-    let titleElement = document.createElement("h1");
-    let titleStrong = document.createElement("strong");
-    titleStrong.textContent = title;
-    titleElement.appendChild(titleStrong); //creates bold p for title
-    
-    let authorElement = document.createElement("p"); //creates p for author
-    authorElement.textContent = `by ${author}`; 
-        
-    let publishedElement = document.createElement("p"); //creates p for published year
-    publishedElement.textContent = `Published: ${published}`;
-    
-    let readElement = document.createElement("p"); //creates p for read status
-    readElement.textContent = `Read: ${hasRead}`;
-    
+    let individualBookId = individualBook.dataset.unique; //the unique id is stored in a variable
+    // console.log(individualBookId);
 
-    //adds all the p into the div
-    singleBookInfo.appendChild(titleElement);
-    singleBookInfo.appendChild(authorElement);
-    singleBookInfo.appendChild(publishedElement);
-    singleBookInfo.appendChild(readElement);
+    //console.log("unique" in individualBook.dataset); //"unique" data-attribute exist in individualBookId.dataset //true
 
-    let removeBtn = document.createElement('button');
-    removeBtn.classList.add('removeBtn');
-    removeBtn.textContent = 'Remove Book';
-    singleBookInfo.appendChild(removeBtn); //remove button added to the new book card
+    //find the index of the object that has the "unique id" of the book on which the remove button was clicked
+    let index = myLibrary.findIndex(book => book["id"] === individualBookId); //finds the index of the object inside the array
 
-    //add the div to the body
-    container.appendChild(singleBookInfo);
-    
-    const singleBookUniqueId = library[library.length - 1].uniqueId; //unique id of the newly added book
-    
-    singleBookInfo.setAttribute('data-uniqueId', singleBookUniqueId); //unique id of the book set to the div
-    removeBtn.setAttribute('data-uniqueId', singleBookUniqueId); //unique id of the set to the remove button
+    // console.log(index);
 
-    console.log(title, author, published, hasRead, singleBookUniqueId);
+    //remove the object from the array
+    if(index !== -1) {
+        myLibrary.splice(index, 1);
+    }
 
-    //resets the form of previous inputs
-    document.getElementById("bookForm").reset();
+    //remove book card from DOM
+    individualBook.remove();
+
+    alert("Book removed!");
+});
+
+//function added to Book prototype 
+Book.prototype.toggleRead = function() {
+    const current = this.hasRead.toLowerCase();
+
+    if(current === "yes") {
+        this.hasRead = "No";
+    }else if(current === "no") {
+        this.hasRead = "Yes";
+    }
+}
+
+mainContainer.addEventListener("click", (e) => {
+    if(!e.target.classList.contains("toggleBtn")) //checks whether the button clicked contains the class "toggleBtn"
+        return;
+        // else(alert("toggle btn clicked"));
+
+    //search for the unique id related to the card in which the "toggle read status" button is clicked
+    let individualBook = e.target.closest("[data-unique]"); //finds the nearest element that has a "data-unique" attribute
+    // console.log(individualBook);
+
+    let individualBookId = individualBook.dataset.unique; //the unique id is stored in a variable
+    // console.log(individualBookId);
+
+    //find the object that has the "unique id" of the book on which the toggle button was clicked
+    let index = myLibrary.find((item => item.id === individualBookId));
+    // console.log(index.hasRead); //read status before function is called
+
+    if(!index)
+        return;
+    else {
+        index.toggleRead();
+    }
+
+    // console.log(index["hasRead"]); //read status after function is called
+    // console.log(index);
+
+    individualBook.querySelector("div > p:last-of-type").textContent = `Read status: ${index.hasRead}`;
 });
